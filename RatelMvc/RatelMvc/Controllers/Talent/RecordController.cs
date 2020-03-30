b09962f3721faa66a913ee86014cf4e7f5896b37
王良昊 <@DESKTOP-BR31NAV>
2020-03-30 19:51:27 +0800

    3月30号王良昊推送控制器

---
+++ RatelMvc/RatelMvc/Controllers/Talent/RecordController.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models.Talent;
+using Newtonsoft.Json;
+
+namespace RatelMvc.Controllers.Talent
+{
+    public class RecordController : Controller
+    {
+        HttpClientHelper helper = new HttpClientHelper("http://localhost:51845");
+        // GET: Record
+        public ActionResult AddRecoed()
+        {
+            return View();
+        }
+        [HttpPost]
+        public void AddRecoed(RecordModel model)
+        {
+            string str = helper.Post("/api/Record/AddRecoed", JsonConvert.SerializeObject(model));
+            int i = int.Parse(str);
+            if (i > 0)
+            {
+                Response.Write("<script>alert('添加成功');location.href='/Record/Index/'</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('添加成功')</script>");
+            }
+        }
+        [HttpGet]
+        public ActionResult Index()
+        {
+            string json = helper.Get("api/Record/ShowRecoed");
+            List<RecordModel> list = JsonConvert.DeserializeObject<List<RecordModel>>(json);
+            return View(list);
+        }
+        [HttpDelete]
+        public void DeleteRecoed(int Id)
+        {
+            string str = helper.Delete("api/Record/DeleteRecoed/?Id=" + Id);
+
+            int i = int.Parse(str);
+
+            if (i > 0)
+            {
+                Response.Write("<script>alert('删除成功');location.href='/Record/Index/';</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('删除失败');location.href='/Record/Index/';</script>");
+            }
+        }
+
+    }
+}
