6a008599733348f30e7032d899893cdb1d6d05e5
guan peng <guan peng@DESKTOP-KB5R02M>
2020-04-07 07:19:13 +0800

    界面

---
+++ RatelMvc/RatelMvc/Controllers/Reimbursement/ReimController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RatelMvc.Controllers.Reimbursement
+{
+    public class ReimController : Controller
+    {
+        // GET: Reim
+        public ActionResult add()
+        {
+            return View();
+        }
+        public ActionResult show()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
